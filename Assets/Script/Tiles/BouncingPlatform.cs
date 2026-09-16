b542fd49c0b0e48727c977e32a0dd276ac29e80d
0x16-0x22-0x0A <0x16-22_022@protonmail.com>
2026-09-16 13:26:03 +0200

    Fix BouncingPlatform.cs and rename associated prefab, rename MovingPlatform.prefab

---
+++ Assets/Script/Tiles/BouncingPlatform.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class BouncingPlatform : MonoBehaviour
+{
+    private Rigidbody2D _playerRigidBody;
+
+    public float BounceForce;
+
+    public void Awake()
+    {
+	if (!SceneHelper.TryFindGameObjectWithTagInScene("Player", out var player))
+		throw new Exception("Object with tag \"Player\" not found.");
+        _playerRigidBody = Player.GetComponent<Rigidbody2D>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _playerRigidBody.AddForce(new Vector2(0, BounceForce * 10), ForceMode2D.Impulse);
+        }
+    }
+}
