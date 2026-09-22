48799be1f74153a3dd00daed0498d766244362be
Dracosomething <larsfalk08@gmail.com>
2026-09-22 14:10:26 +0200

    Made WalkingEnemy.cs conform to conventions, shortened WalkingEnemy.cs ai code.

--- Assets/Script/Enemies/WalkingEnemy.cs
+++ Assets/Script/Enemies/WalkingEnemy.cs
@@ -1,56 +1,36 @@
-using System.Linq;
 using UnityEngine;
 
 public class WalkingEnemy : Enemy
 {
-    public Transform[] PatrolPoints;
+    [SerializeField] private Vector2 _endPosition;
+    private Vector2 _startPosition;
+    private Vector2 _targetPosition;
 
-    private void FixedUpdate()
+    protected override void Awake()
     {
-        DrawDebug();
-    }
-    
-    protected override void Update()
-    {
-        base.Update();
-        Patrol();
+        base.Awake();
+        
+        // Always first move towards end position.
+        _startPosition = transform.position;
+        _targetPosition = _endPosition;
     }
 
     protected override void Move()
     {
-        throw new System.NotImplementedException();
-    }
-
-    private void Patrol()
-    {   // We check if there are any patrol points
-        if (PatrolPoints.Length == 0) 
-            return;
-        Transform targetPoint = PatrolPoints.First();
-        var targetPosition = (Vector2)targetPoint.position;
-        
         RigidBody.position = Vector2.MoveTowards(
             current: RigidBody.position, 
-            target: targetPosition,
+            target: _targetPosition,
             maxDistanceDelta: Speed * Time.fixedDeltaTime);
-        
-        float distanceToTargetPosition = Vector2.Distance(RigidBody.position, targetPosition);
-        
-        if (distanceToTargetPosition <= 1)
-        {
-            for (var i = 0; i < PatrolPoints.Length - 1; i++)
-            {
-                PatrolPoints[i] = PatrolPoints[i + 1];
-            }
-            PatrolPoints[PatrolPoints.Length - 1] = targetPoint;
-        }
+
+        if (RigidBody.position == _targetPosition)
+            SwitchTargetPosition();
     }
 
-    private void DrawDebug()
+    private void SwitchTargetPosition()
     {
-        for (int i = 0; i < PatrolPoints.Length; i++)
-        {
-            Debug.DrawLine(transform.position, PatrolPoints[i].position, Color.green);
-        }
-        Debug.DrawRay(PatrolPoints[0].position, Vector2.up * 2, Color.red);
+        if (_targetPosition == _endPosition)
+            _targetPosition = _startPosition;
+        else
+            _targetPosition = _endPosition;
     }
 }
