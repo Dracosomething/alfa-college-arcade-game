369d472073cd6062cc52d7732480d6e7dcc2a3d5
Dracosomething <larsfalk08@gmail.com>
2026-09-22 12:06:18 +0200

    Removed unused stuff from FlyingEnemy.cs.

--- Assets/Script/Enemies/FlyingEnemy.cs
+++ Assets/Script/Enemies/FlyingEnemy.cs
@@ -8,12 +8,9 @@ public class FlyingEnemy : Enemy
 {
     [SerializeField] private float _wanderRadius = 1f;
     [SerializeField] private float _sightDistance = 1f;
-    private bool _hasSeenPlayerRecently;
     [SerializeField] private CustomTimeSpan _newWanderPositionCooldownDuration = new(seconds: 1);
     private CooldownWrapper _newWanderPositionCooldown;
     private NavMeshAgent _agent;
-    private Coroutine _wanderCoroutine;
-    private Coroutine _forgetCoroutine;
     private Vector2 _spawnPosition;
 
     protected override void Awake()
@@ -30,15 +27,6 @@ public class FlyingEnemy : Enemy
 
         _newWanderPositionCooldown = this.AddComponent<CooldownWrapper>();
         _newWanderPositionCooldown.InitializeCooldownWrapper(_newWanderPositionCooldownDuration);
-        
-        // _wanderCoroutine = StartCoroutine(WanderRoutine());
-    }
-
-    protected override void Update()
-    {
-        base.Update();
-        
-        // CheckIfPlayerCanBeSeen();
     }
 
     protected override void Move()
@@ -85,67 +73,4 @@ public class FlyingEnemy : Enemy
         
         return positionToWanderTo;
     }
-    
-    // private void CheckIfPlayerCanBeSeen()
-    // {
-    //     Vector3 currentEnemyPosition = transform.position;
-    //     var isPlayerVisible = false;
-    //     
-    //     var directionToPlayer = (Vector2)(Player.transform.position - currentEnemyPosition).normalized;
-    //     RaycastHit2D[] allObjectsHitByRay = Physics2D.RaycastAll(currentEnemyPosition, directionToPlayer, _sightDistance);
-    //     Debug.DrawLine(currentEnemyPosition, currentEnemyPosition + (Vector3)directionToPlayer * _sightDistance);
-    //
-    //     if (allObjectsHitByRay.Length != 0)
-    //     {
-    //         isPlayerVisible = allObjectsHitByRay
-    //             .Where(raycastHitObjectInfo => (bool)raycastHitObjectInfo.collider) // Filters out all RaycastHit2D where the collider is null
-    //             .Where(raycastHitObjectInfo => (bool)raycastHitObjectInfo.collider.gameObject) // Filters out all RaycastHit2D where collider.gameObject is null
-    //             .Any(raycastHitObjectInfo => raycastHitObjectInfo.collider.CompareTag("Player")); // Checks if any of the collided objects are the player.
-    //     }
-    //
-    //     if (isPlayerVisible)
-    //     {
-    //         _targetPosition = Player.transform.position;
-    //         _hasSeenPlayerRecently = true;
-    //
-    //         if (_forgetCoroutine != null)
-    //         {
-    //             StopCoroutine(_forgetCoroutine);
-    //             _forgetCoroutine = null;
-    //         }
-    //     }
-    //     else if (_hasSeenPlayerRecently && _forgetCoroutine == null)
-    //         _forgetCoroutine = StartCoroutine(ForgetPlayer());
-    // }
-    //
-    // private IEnumerator WanderRoutine()
-    // {
-    //     while (true)
-    //     {
-    //         if (!_hasSeenPlayerRecently)
-    //         {
-    //             Vector2 randomPointInWanderRadius = Random.insideUnitCircle * _wanderRadius;
-    //             Vector3 directionToWonderTo = _spawnPosition + new Vector3(randomPointInWanderRadius.x, randomPointInWanderRadius.y, _spawnPosition.z);
-    //
-    //             // If we can find a mesh we can move to.
-    //             if (NavMesh.SamplePosition(sourcePosition: directionToWonderTo, out NavMeshHit foundMesh, _wanderRadius, NavMesh.AllAreas))
-    //             {
-    //                 _targetPosition = new Vector3(foundMesh.position.x, foundMesh.position.y, _spawnPosition.z);
-    //             }
-    //             else
-    //             {
-    //                 _targetPosition = new Vector3(directionToWonderTo.x, directionToWonderTo.y, _spawnPosition.z);
-    //             }
-    //         }
-    //
-    //         yield return new WaitForSeconds(_newWanderPositionCooldown);
-    //     }
-    // }
-    //
-    // private IEnumerator ForgetPlayer()
-    // {
-    //     yield return new WaitForSeconds(_forgetDelay);
-    //     _hasSeenPlayerRecently = false;
-    //     _forgetCoroutine = null;
-    // }
 }
