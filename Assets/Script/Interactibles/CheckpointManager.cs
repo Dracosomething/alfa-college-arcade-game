0c20262749db234f7844f5a02e928f18cbab6c3b
Dracosomething <larsfalk08@gmail.com>
2026-09-23 09:45:39 +0200

    Renamed BenchInteractible to CheckpointManager.

--- Assets/Script/Interactibles/CheckpointManager.cs
+++ Assets/Script/Interactibles/CheckpointManager.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 
-public class BenchInteractible : MonoBehaviour
+public class CheckpointManager : MonoBehaviour
 {
     public void SetBenchAsCheckpoint(
         OldPlayerController/* replace monobehaviour with player when player controller is finished */ player) =>
         player._subCheckPointPosition = transform.position;
 }
+
